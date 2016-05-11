bb99b387c1c03740ec05fc9ab7e8217767593dbe
Warren Strange <warren.strange@gmail.com>
2016-05-11 16:47:55 -0600

    added IG sample config

--- helm/helm.sh
+++ helm/helm.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+# source this
 export HELM_DIR="~/src/go/src/github.com/kubernetes/helm"
 
 alias helm="$HELM_DIR/bin/helm"
 alias tiller="$HELM_DIR/bin/tiller"
+
+# init helm
+echo helm init -n default
 
