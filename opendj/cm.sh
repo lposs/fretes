bd73ccd0eb7b39352092260ae38915bd97b77efd
Warren Strange <warren.strange@gmail.com>
2016-07-08 17:11:50 -0600

    dj pet

--- opendj/cm.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete configmap djconfig
-kubectl create configmap djconfig --from-file=djconfig=setup.sh 
-
--- opendj/d.sh
+++ opendj/d.sh
@@ -6,9 +6,6 @@ kubectl delete petset opendj
 kubectl delete pod opendj-0
 kubectl delete pod opendj-1
 
-kubectl delete configmap djconfig
-kubectl create configmap djconfig --from-file=djconfig=setup.sh --from-file=replicate=replicate.sh
-
 
 echo "Run the following"
 echo kubectl create -f opendj.yaml
