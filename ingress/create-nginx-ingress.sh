e9ca80cb591719de50f248ceb8d81288ddfb716d
Warren Strange <warren.strange@gmail.com>
2016-09-30 19:17:06 -0600

    new base image

--- ingress/create-nginx-ingress.sh
+++ ingress/create-nginx-ingress.sh
@@ -4,8 +4,11 @@
 
 P="https://raw.githubusercontent.com/kubernetes/contrib/master/ingress/controllers/nginx"
 
-curl -s "$P/examples/default-backend.yaml" | kubectl create -f -
+#curl -s "$P/examples/default-backend.yaml" | kubectl create -f -
 
 kubectl expose rc default-http-backend --port=80 --target-port=8080 --name=default-http-backend
 
-curl -s "$P/examples/default/rc-default.yaml" | kubectl create  -f -
+#curl -s "$P/examples/default/rc-default.yaml" | kubectl create  -f -
+
+kubectl create -f rc-default.yaml
+
