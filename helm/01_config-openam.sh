f31aaae6fc3ccbcb12d237edad58787d00ec58d4
Warren Strange <warren.strange@gmail.com>
2016-11-15 17:07:02 -0400

    GKE commented out by default

--- helm/01_config-openam.sh
+++ helm/01_config-openam.sh
@@ -2,8 +2,10 @@
 # Script that configures DJ and then runs the openam-install chart
 # At completion openam should be ready to go
 
+# Uncomment to run on GKE
+#GKE="-f gke.yaml"
 
-helm install --name opendj opendj
+helm install ${GKE} --name opendj opendj
 
 echo "Giving DJ some time to start"
 
@@ -12,7 +14,7 @@ echo "Giving DJ some time to start"
 sleep 180
 
 echo "Configuring OpenAM"
-helm install --name openam-install openam-install
+helm install ${GKE} --name openam-install openam-install
 
 echo "Waiting for the configurator "
 
--- helm/02_start-openam.sh
+++ helm/02_start-openam.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Comment this out if not deploying to GKE
-GKE="-f gke.yaml"
+#GKE="-f gke.yaml"
 
 #DRYRUN="--dry-run --debug"
 
 echo "Deleting the install chart as it is no longer required "
 # We can delete the install chart now - its job is done
-#helm delete openam-install --purge
+helm delete openam-install --purge
 
 echo "Creating the OpenAM runtime"
 helm install ${GKE} ${DRYRUN} --name openam openam-runtime
